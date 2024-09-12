616966b74283bdbf75e0045f61fdd0750c26e7ab
Mazo667 <maxifava667@gmail.com>
2024-09-12 18:56:53 -0300

    Intento de implementacion de diccionarios de consultas

--- BBDD2/TP/TP1/diccionario_consultas.py
+++ BBDD2/TP/TP1/diccionario_consultas.py
@@ -1,4 +1,4 @@
 consultas = {
-    '':'',
+    'usuarios conectados, IP y consulta':'SELECT username AS Usuario,client_addr AS Cliente IP,query AS Consulta,state AS Estado,query_start AS Consulta inicio tiempo FROM pg_stat_activity;',
     '':''
 }
--- BBDD2/TP/TP1/main.py
+++ BBDD2/TP/TP1/main.py
@@ -1,26 +1,35 @@
 import psycopg2
 import conexion_bdd
 import ejecutar_consulta
+import diccionario_consultas
+
 from psycopg2 import sql
 
 def main():
-    connection = conexion_bdd()
+    connection = conexion_bdd.conexion_bdd()
     if connection:
         print("Conexión exitosa a la base de datos.")
         while True:
             print("-------------------------")
             print("---MENU CONSULTAS---")
-            print("1- Obtener todas las bases de datos disponibles")
-            print("2- Obtener los Roles que se puede conectar al servidor")
-            print("3- ")
+            print("1- Obtener los usuarios conectados, IP y consulta.")
+            print("2- Obtener el tamaño de la base de datos del servidor.")
+            print("3- Obtener el tamaño de una tabla.")
+            print("4- Obtener detalles de las tablas. orden de los campos, nombre y tipo de dato.")
+            print("5- Obtener el tamaño de las tablas de un esquema.")
+            print("6- Listado por BBDD del tamaño total de la BBDD, el nombre del esquema, el nombre de la tabla, el tamaño  total de la tabla, el tamaño de la tabla y el tamaño del índice")
+            print("7- Consulta personalizada.")
             
-            query = input("Escribe tu consulta SQL (o 'salir' para terminar): ")
-            if query.lower() == 'salir':
+            option = input("Escribe la opcion: (o 'salir' para terminar): ")
+            if option.lower() == 'salir':
                 break
-            ejecutar_consulta(connection, query)
+            #ejecutar_consulta(connection, query)
+            match option:
+                case '1':
+                    query = diccionario_consultas.consultas['usuarios conectados, IP y consulta']
+                    ejecutar_consulta(connection, query)
         connection.close()
         print("Conexión cerrada.")
 
 if __name__ == '__main__':
     main()
-    
